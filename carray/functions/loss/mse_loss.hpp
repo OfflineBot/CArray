bd4481a5edcea2634cd96f0b8d2f92a02188199b
OfflineBot <FlintStoneLG@gmail.com>
2024-04-02 21:24:33 +0200

    readme

--- carray/functions/loss/mse_loss.hpp
+++ carray/functions/loss/mse_loss.hpp
@@ -7,5 +7,12 @@
 
 #include "../functions.hpp"
 
+template<typename T>
+T mse_loss(CArray::Array2<T> &output, CArray::Array2<T> &truth) {
+    CArray::Array2<T> error = output - truth;
+    error *= error;
+    CArray::Array1<T> arr1_mean = error.mean();
+    return arr1_mean.mean();
+}
 
 #endif //CORRECT_CARRAY_MSE_LOSS_HPP
--- main.cpp
+++ main.cpp
@@ -5,16 +5,36 @@ using namespace CArray;
 
 int main() {
 
+    int iterations = 10000;
+    float learning_rate = 0.01;
+    int input_size = 2;
+    int hidden_size = 10;
+    int output_size = 1;
+
+    Array2<float> data_input = { {1, 1}, {0, 0}, {1, 0}, {0, 1} };
+    Array1<float> input_mean = data_input.mean();
+    Array1<float> input_std = data_input.std();
+    Array2<float> norm_input = (data_input - input_mean) / input_std;
+
+    Array2<float> data_output = { {0}, {0}, {1}, {11} };
+    Array1<float> output_mean = data_output.mean();
+    Array1<float> output_std = data_output.std();
+    Array2<float> norm_output = (data_output - output_mean) / output_std;
+
+    Array2<float> new_input = data_input;
+    Array2<float> new_input_norm = (new_input - input_mean) / input_std;
+
+    float min = -1.0; float max = 1.0;
+    Array2<float> w1(input_size, hidden_size);
+    Array1<float> b1(hidden_size);
+    Array2<float> w2(hidden_size, output_size);
+    Array1<float> b2(output_size);
+    w1.random_uniform(min, max);
+    b1.random_uniform(min, max);
+    w2.random_uniform(min, max);
+    b2.random_uniform(min, max);
 
 
-    Array2<float> array2 = {
-            {1, 2, 3},
-            {4, 5, 6}
-    };
-    Array1<float> array1(3);
-    array1 += 1;
-    array2 += array1;
-    array2.print();
 
     return 0;
 }
